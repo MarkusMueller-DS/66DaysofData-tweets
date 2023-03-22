5303ea9a20b184d09cef8dfe3b8e79441495d17b
Markus Müller <markus.j.mueller94@icloud.com>
2023-03-22 09:48:23 +0100

    improved data pipeline

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # 66DaysofData tweets Analysis (WIP)
+
 ##### Analysis of tweets from the #66DaysofData community/challenge
 
 ![wordcloud](reports/figures/test_vertical.png)
@@ -6,37 +7,41 @@
 <a href="https://markusmueller-ds.github.io/portfolio/66days_analysis.html">Link</a> to website with more informations.
 
 ### Challenges
-- Get historical tweets isn't easy. 
+
+- Get historical tweets isn't easy.
 - Twitter-premium API has a limit query amount per month
 - Oher Twitter APIs can only retrieve tweets with full-text up to one week
 
 ### Files
+
 - data: folder for raw, processed and final data
 - notebooks: folder containing all the jupyter notebooks
 - scripts: folder containing every python script for data collecting, preprocessing and analysis
 
 ### ToDO
+
 - make a more simpler data gathering pipeline. At this point I have to do various steps in differrent environments to have a final DataFrame with all the data
 - deploy word cloud generator for the participants (work locally)
 
 ### First results
+
 - How many total tweets?
-    - 33276
+  - 33276
 - How many unique participants?
-    - 1625
+  - 1625
 - Basic statistics on tweets?
-    - mean: 61
-    - median: 54
-    - max: 165
-    - min: 8
+  - mean: 61
+  - median: 54
+  - max: 165
+  - min: 8
 - Day with most tweets / Day with least tweets
-    - min tweets: 8 tweets on 19th and 20th of Decemer 2020
-    - max tweets: 2nd of September 2020, (one day after the start)
+  - min tweets: 8 tweets on 19th and 20th of Decemer 2020
+  - max tweets: 2nd of September 2020, (one day after the start)
 - Who was the most active user?
-    - paulapivat (173 tweets)
+  - paulapivat (173 tweets)
 
 ### Wordcloud Streamlit-App
+
 ![wordcloud_app](reports/figures/Word_Cloud_App.png)
 
 ### Files
-
--- scripts/get_tweets_tweepy.py
+++ scripts/get_tweets_tweepy.py
@@ -1,6 +1,3 @@
-#####
-# Script requiers a user to change the dates for the output files 
-
 # imports
 import os
 import tweepy as tw
@@ -8,12 +5,16 @@ import pandas as pd
 import pickle
 import time
 from datetime import date
+from dotenv import load_dotenv
+
+# load environment variables (keys)
+load_dotenv()
 
 # define keys
-consumer_key = 'XXX'
-consumer_secret = 'XXX'
-access_token = 'XXX'
-access_token_secret = 'XXX'
+consumer_key = os.environ.get('CONSUMER_KEY')
+consumer_secret = os.environ.get('CONSUMER_SECRET') 
+access_token = os.environ.get('ACCESS_TOKEN')
+access_token_secret =  os.environ.get('ACCESS_TOKEN_SECRET')
 
 # set keys
 auth = tw.OAuthHandler(consumer_key, consumer_secret)
@@ -28,17 +29,20 @@ date_today = date.today()
 search_word = '#66daysofdata -filter:retweets' 
 
 # collect tweets
-tweets = tw.Cursor(api.search, q=search_word, lang='en', tweet_mode="extended").items()
+tweets = tw.Cursor(api.search_tweets, q=search_word, lang='en', tweet_mode="extended").items()
 
 # append the tweets in a list
 list_tweets = [] # empty list
 for tweet in tweets:
     list_tweets.append(tweet)
-    
+"""
+with open('respnse.txt', 'w') as output:
+    output.write(str(list_tweets))
+"""
+
 # access the tweets 
 # the tweets are in a json for each tweet
 # list_tweets[1]._json will give you the json with the information to the first tweet
-    
 
 # create empty lists for each relevant key 
 tweet_id = []
@@ -65,10 +69,11 @@ d = {'tweet_id': tweet_id, 'user_id': user_id, 'user_name': user_name, 'created_
      'retweets': retweets, 'favorite': favorite}
 data_df_tweepy = pd.DataFrame(d)
 
+print(f"got {data_df_tweepy.shape[0]} tweets")
+
 # file name
 file_name_csv = 'tweets_' + date_today.strftime('%d.%m.%Y') + '_tweepy.csv'
 file_name_pkl = 'tweetsData_' + date_today.strftime('%d.%m.%Y') + '_tweepy.pkl'      
-
 
 # saves the DataFrame as a csv:
 data_df_tweepy.to_csv(file_name_csv, index=False)
@@ -77,6 +82,26 @@ data_df_tweepy.to_csv(file_name_csv, index=False)
 with open(file_name_pkl, 'wb') as f:
     pickle.dump(list_tweets, f)
 
+PATH_DATA = "data/final/tweets_66DaysofData.csv"
+    
+# automatically add to csv-db
+if os.path.isfile(PATH_DATA):
+    print('add new data to csv')
+    final_frame = pd.read_csv(PATH_DATA)
+    print(f'rows of csv: {final_frame.shape[0]}')
+    frames = [data_df_tweepy, final_frame]
+    result = pd.concat(frames)
 
+    # make datetime column to sort values by datetime
+    result['created_at'] = pd.to_datetime(result['created_at'])
 
+    # sort by date
+    result.sort_values(['created_at'], inplace=True)
 
+    # drop for duplicate rows
+    results = result.drop_duplicates(subset=['full_text'], keep='last')
+    print(f'rows after merging: {results.shape[0]}')
+else:
+    data_df_tweepy.to_csv(PATH_DATA, index=False)
+
+print('done')
